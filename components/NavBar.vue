b54f9392e9a51670c27206bcb029e6387e9a4a80
Uma Victor <umavictor11@gmail.com>
2021-09-28 21:12:26 +0100

    add navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+<nav class="bg-gray-800">
+  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div class="relative flex items-center justify-between h-16">
+        <div><h1 class="text-white text-3xl">Eleventy</h1></div>
+        <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          type="search" name="search" placeholder="Search">
+        <button class="inline-flex items-center justify-center px-4 py-2 rounded-md text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">Add Event</button>
+    </div>
+  </div>
+
+  <!-- Mobile menu, show/hide based on menu state. -->
+  <div class="sm:hidden" id="mobile-menu">
+    <div class="px-2 pt-2 pb-3 space-y-1">
+    </div>
+  </div>
+</nav>
+</template>
